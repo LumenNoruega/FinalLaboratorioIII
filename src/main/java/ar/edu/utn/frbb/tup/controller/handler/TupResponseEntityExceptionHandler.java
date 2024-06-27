39665025cb8ae283919f0b4e2149b2bfe96c0012
luciano.salotto <lsalotto@gmail.com>
2024-06-27 19:38:57 -0300

    Convierte en aplicación spring boot. Y agrega los dos controllers

---
+++ src/main/java/ar/edu/utn/frbb/tup/controller/handler/TupResponseEntityExceptionHandler.java
@@ -0,0 +1,53 @@
+package ar.edu.utn.frbb.tup.controller.handler;
+
+import ar.edu.utn.frbb.tup.model.exception.TipoCuentaAlreadyExistsException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class TupResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value
+            = {TipoCuentaAlreadyExistsException.class})
+    protected ResponseEntity<Object> handleMateriaNotFound(
+            TipoCuentaAlreadyExistsException ex, WebRequest request) {
+        String exceptionMessage = ex.getMessage();
+        CustomApiError error = new CustomApiError();
+        error.setErrorMessage(exceptionMessage);
+        return handleExceptionInternal(ex, error,
+                new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(value
+            = { IllegalArgumentException.class, IllegalStateException.class })
+    protected ResponseEntity<Object> handleConflict(
+            RuntimeException ex, WebRequest request) {
+        String exceptionMessage = ex.getMessage();
+        CustomApiError error = new CustomApiError();
+        error.setErrorCode(1234);
+        error.setErrorMessage(exceptionMessage);
+        return handleExceptionInternal(ex, error,
+                new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        if (body == null) {
+            CustomApiError error = new CustomApiError();
+            error.setErrorMessage(ex.getMessage());
+            body = error;
+        }
+
+        return new ResponseEntity(body, headers, status);
+    }
+
+}
